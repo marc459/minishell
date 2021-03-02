62896501007c1ee37d87666517caec633049fd41
marc459 <39402167+marc459@users.noreply.github.com>
2021-03-02 15:45:46 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,39 @@
+  
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+
+int		main(void)
+{
+	pid_t	pid;
+	char	comando[64];
+	printf("Minishell: ");
+	memset(comando, '\0', 64);
+	scanf("%s", comando);
+	while (strcmp(comando, "exit"))
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			execlp(comando, comando, NULL);
+			printf("comando no valido\n");
+			exit(0);
+		}
+		else
+		{
+			if (pid > 0)
+			{
+				waitpid(pid, 0, 0);
+				printf("Minishell: ");
+				memset(comando, '\0', 64);
+				scanf("%s", comando);
+			}
+			else
+				perror("Error Fork\n");
+		}
+	}
+	exit(0);
+}

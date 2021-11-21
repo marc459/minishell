afd236c99c50680bd02597afe3d6568e12099901
Mexil77 <mendel_77@hotmail.com>
2021-11-22 00:39:45 +0100

    Algo

--- srcs/parse/ft_args3.c
+++ srcs/parse/ft_args3.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 21:12:27 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/11/21 16:28:50 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/11/22 00:32:45 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,46 +47,32 @@ void	ft_iniarg(t_general *g, size_t *j, char *str)
 		g->args[(*j)++].type = 3;
 }
 
-void	ft_erasequotes(char **strquotes)
+void	ft_dropquotes(t_general *g, size_t i)
 {
-	char	*str;
-	size_t	i;
-
-	i = 0;
-	str = calloc(sizeof(char), ft_strlen(*strquotes - 1));
-	if (!str)
-		exit(0);
-	while (strquotes[0][++i] && ft_strlen(*strquotes) > 2
-		&& i < ft_strlen(*strquotes) - 1)
-		str[i - 1] = strquotes[0][i];
-	free(*strquotes);
-	*strquotes = ft_strdup(str);
-	free(str);
-}
-
-void	ft_checkquotes(t_general *g, size_t i)
-{
-	char	**split;
-	size_t	join;
 	size_t	j;
+	size_t	k;
+	char	*aux;
+	char	*str;
 
-	split = ft_split(g->args[i].content, ' ');
 	j = -1;
-	join = 0;
-	while (split[++j])
+	k = 0;
+	aux = g->args[i].content;
+	if (ft_getposition(aux, '\"') > ft_getposition(aux, '\''))
 	{
-		if (ft_findchar(split[j], '\"')
-			|| ft_findchar(split[j], '\''))
-		{
-			ft_erasequotes(&split[j]);
-			join++;
-		}
+		str = ft_trimchar(aux, '\"');
+		printf("str : %s\n", str);
+		aux = str;
+		str = ft_revtrimchar(str, '\"');
+		printf("str : %s\n", str);
 	}
-	if (join)
+	else if (ft_getposition(aux, '\"') < ft_getposition(aux, '\''))
 	{
-		free(g->args[i].content);
-		g->args[i].content = ft_splitjoin(split, ' ');
+		str = ft_trimchar(aux, '\'');
+		aux = str;
+		str = ft_revtrimchar(str, '\'');
 	}
+	free (aux);
+	g->args[i].content = str;
 }
 
 void	ft_refactquotes(t_general *g)
@@ -97,5 +83,5 @@ void	ft_refactquotes(t_general *g)
 	while (++i < g->argssize)
 		if (ft_findchar(g->args[i].content, '\"')
 			|| ft_findchar(g->args[i].content, '\''))
-			ft_checkquotes(g, i);
+			ft_dropquotes(g, i);
 }

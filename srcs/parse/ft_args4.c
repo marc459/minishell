3ce3d92d7f4192560160b353c1a784f34efc65d2
Mexil77 <mendel_77@hotmail.com>
2021-11-23 23:37:30 +0100

    Resolviendo prblema de expansion sin norma

--- srcs/parse/ft_args4.c
+++ srcs/parse/ft_args4.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:01:26 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/11/23 16:47:58 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/11/23 23:35:25 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,27 +39,57 @@ char	*ft_joinenv(char *str)
 	return (final);
 }
 
+char	*ft_getenv(char *str, size_t *last, size_t *j)
+{
+	size_t	i;
+	char	*env;
+	char	aux2;
+
+	i = 0;
+	env = NULL;
+	str++;
+	*last += 1;
+	*j += 1;
+	while (str[i] && str[i] != ' ' && str[i] != '\'' && str[i] != '\"')
+	{
+		*last += 1;
+		*j += 1;
+		i++;
+	}
+	env = ft_substr(str, 0, i);
+	return (ft_strdup(getenv(env)));
+}
+
 void	ft_expand(t_general *g, size_t i)
 {
-	char	**split;
 	char	*aux;
 	char	*str;
 	size_t	j;
+	size_t	last;
 
-	split = ft_split(g->args[i].content, ' ');
-	j = -1;
-	while (split[++j])
+	last = 0;
+	if ((ft_getposition(g->args[i].content, '\"') < ft_getposition(g->args[i].content, '\''))
+		|| (ft_getposition(g->args[i].content, '\"') == ft_getposition(g->args[i].content, '\'')))
 	{
-		if (ft_findchar(split[j], '$') && ft_findchar(split[j], '\''))
+		j = -1;
+		while (g->args[i].content[++j])
 		{
-			aux = split[j];
-			split[j] = ft_joinenv(split[j]);
-			free (aux);
+			if (g->args[i].content[j] == '$')
+			{
+				str = ft_substr(g->args[i].content, last, j);
+				last = j;
+				aux = str;
+				str = ft_strjoin(str, ft_getenv(&g->args[i].content[j], &last, &j));
+				free (aux);
+			}
 		}
+		aux = str;
+		str = ft_strjoin(str, ft_substr(g->args[i].content, last, j));
+		free (aux);
+		free(g->args[i].content);
+		g->args[i].content = str;
 	}
-	free(g->args[i].content);
-	g->args[i].content = ft_splitjoin(split, ' ');
-	ft_freedouble(split);
+	printf("content : %s\n", g->args[i].content);
 }
 
 void	ft_expvar(t_general *g)

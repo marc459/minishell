ee0383336570789592c7413e73156fd51955e7cd
Mexil77 <mendel_77@hotmail.com>
2021-11-24 03:29:13 +0100

    Agregando actualizacion funcional de expacion de variables de entorno

--- srcs/parse/ft_args4.c
+++ srcs/parse/ft_args4.c
@@ -6,38 +6,11 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:01:26 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/11/23 23:35:25 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/11/24 03:23:35 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-
-char	*ft_joinenv(char *str)
-{
-	char	*aux;
-	char	*aux2;
-	char	*aux3;
-	char	*final;
-
-	aux = ft_strdup(str);
-	aux += (ft_getposition(str, '$') + 1);
-	aux2 = aux;
-	if (ft_findchar(aux, '\''))
-		aux = ft_revtrimchar(aux, '\'');
-	final = ft_substr(str, 0, ft_getposition(str, '$'));
-	aux3 = final;
-	final = ft_strjoin(final, getenv(aux));
-	free (aux3);
-	if (ft_findchar(aux2, '\''))
-	{
-		aux3 = final;
-		final = ft_strjoin(final, "\'");
-		free (aux3);
-	}
-	aux2 -= (ft_getposition(str, '$') + 1);
-	free (aux2);
-	return (final);
-}
 
 char	*ft_getenv(char *str, size_t *last, size_t *j)
 {
@@ -60,36 +33,41 @@ char	*ft_getenv(char *str, size_t *last, size_t *j)
 	return (ft_strdup(getenv(env)));
 }
 
-void	ft_expand(t_general *g, size_t i)
+void	ft_joinenv(char **str)
 {
 	char	*aux;
-	char	*str;
+	char	*line;
 	size_t	j;
 	size_t	last;
 
 	last = 0;
-	if ((ft_getposition(g->args[i].content, '\"') < ft_getposition(g->args[i].content, '\''))
-		|| (ft_getposition(g->args[i].content, '\"') == ft_getposition(g->args[i].content, '\'')))
+	j = -1;
+	while (str[0][++j])
 	{
-		j = -1;
-		while (g->args[i].content[++j])
+		if (str[0][j] == '$')
 		{
-			if (g->args[i].content[j] == '$')
-			{
-				str = ft_substr(g->args[i].content, last, j);
-				last = j;
-				aux = str;
-				str = ft_strjoin(str, ft_getenv(&g->args[i].content[j], &last, &j));
-				free (aux);
-			}
+			line = ft_substr(str[0], last, j);
+			last = j;
+			aux = line;
+			line = ft_strjoin(line, ft_getenv(&str[0][j], &last, &j));
+			free (aux);
 		}
-		aux = str;
-		str = ft_strjoin(str, ft_substr(g->args[i].content, last, j));
-		free (aux);
-		free(g->args[i].content);
-		g->args[i].content = str;
 	}
-	printf("content : %s\n", g->args[i].content);
+	aux = line;
+	line = ft_strjoin(line, ft_substr(str[0], last, j));
+	free (aux);
+	free(str[0]);
+	str[0] = line;
+}
+
+void	ft_expand(t_general *g, size_t i)
+{
+	char	*aux2;
+
+	aux2 = g->args[i].content;
+	if ((ft_getposition(aux2, '\"') < ft_getposition(aux2, '\''))
+		|| (ft_getposition(aux2, '\"') == ft_getposition(aux2, '\'')))
+		ft_joinenv(&g->args[i].content);
 }
 
 void	ft_expvar(t_general *g)

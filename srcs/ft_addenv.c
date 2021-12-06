9f665a19dcb691fc99b663892e031ed5e7b1fa2a
Mexil77 <mendel_77@hotmail.com>
2021-12-06 18:49:27 +0100

    Arregando la presentacion de la variable sin valor con el export

--- srcs/ft_addenv.c
+++ srcs/ft_addenv.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 05:18:15 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/12/05 06:16:46 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/12/06 18:48:35 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,27 +27,43 @@ size_t	ft_checkenvname(char *name)
 	return (1);
 }
 
+size_t	ft_modenv(t_general *g, char *name, char **gvar)
+{
+	size_t	i;
+
+	i = -1;
+	while (g->ownenv[++i])
+	{
+		if (ft_strncmp(name, g->ownenv[i], ft_strlen(name)) == 0
+			&& (g->ownenv[i][ft_strlen(name)] == '='
+			|| !g->ownenv[i][ft_strlen(name)]))
+		{
+			free (g->ownenv[i]);
+			g->ownenv[i] = *gvar;
+			return (1);
+		}
+	}
+	return (0);
+}
+
 void	ft_addenv(t_general *g, char *name, char *val)
 {
 	size_t	i;
 	char	*gvar;
 	char	**newenv;
 
-	gvar = ft_strjoin(name, "=\"");
-	ft_strownjoin(&gvar, val);
-	ft_strownjoin(&gvar, "\"");
-	i = -1;
-	while (g->ownenv[++i])
+	gvar = ft_strdup(name);
+	if (ft_strlen(val))
 	{
-		if (ft_strncmp(name, g->ownenv[i], ft_strlen(name)) == 0
-			&& g->ownenv[i][ft_strlen(name)] == '=')
-		{
-			free (g->ownenv[i]);
-			g->ownenv[i] = gvar;
-			return ;
-		}
+		ft_strownjoin(&gvar, "=\"");
+		ft_strownjoin(&gvar, val);
+		ft_strownjoin(&gvar, "\"");
 	}
+	if (ft_modenv(g, name, &gvar))
+		return ;
 	newenv = calloc(sizeof(char *), (ft_splitlen(g->ownenv) + 2));
+	if (!newenv)
+		return ;
 	i = -1;
 	while (g->ownenv[++i])
 		newenv[i] = ft_strdup(g->ownenv[i]);
@@ -63,20 +79,14 @@ void	ft_checknewenv(t_general *g, char *newenv)
 	char	*name;
 	char	*val;
 
-	i = -1;
+	i = 0;
 	ultima = 0;
-	while (newenv[++i])
-	{
-		if (newenv[i] == '=' || i == ft_strlen(newenv) - 1)
-		{
-			name = ft_substr(newenv, ultima, i - ultima);
-			ultima = i + 1;
-			if (!ft_checkenvname(name))
-				return ;
-			else
-				break ;
-		}
-	}
+	while (newenv[i] && newenv[i] != '=')
+		i++;
+	name = ft_substr(newenv, ultima, i);
+	ultima = i + 1;
+	if (!ft_checkenvname(name))
+		return ;
 	val = ft_substr(newenv, ultima, i - ultima);
 	ultima = i + 1;
 	ft_addenv(g, name, val);

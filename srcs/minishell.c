eeae8db4d735bd5fbbf7c42be611aa3226d7be35
Marcos Santos serrano <santosserranomarcos@gmail.com>
2021-12-22 22:01:14 +0100

    some changes

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 22:32:27 by marcos            #+#    #+#             */
-/*   Updated: 2021/12/22 20:50:37 by msantos-         ###   ########.fr       */
+/*   Updated: 2021/12/22 21:35:09 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,12 @@ void	ft_prompt(t_general *g_m)
 		{
 			ft_inigeneral(g_m);
 			ft_parse(g_m, command);
-			ft_printgeneral(g_m);
+			//ft_printgeneral(g_m);
 			printf("%s< QUINES && MEXIL SHELL >%s\n\n", BCYAN, COLOR_OFF);
 			if (g_m->args)
 				ft_executor(g_m, g_m->ownenv, &pid);
 			free_gminishell(g_m);
-			ft_checkleaksreturn();
+			//ft_checkleaksreturn();
 		}
 	}
 	free(command);

44dce789e069d7cdd3eb419a2cf3bf93681124e3
Marcos Santos serrano <santosserranomarcos@gmail.com>
2021-12-22 20:13:35 +0100

    norminette

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 22:32:27 by marcos            #+#    #+#             */
-/*   Updated: 2021/12/22 18:37:45 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/12/22 20:10:21 by msantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,11 +55,9 @@ void	ft_prompt(t_general *g_m)
 			ft_inigeneral(g_m);
 			ft_parse(g_m, command);
 			ft_printgeneral(g_m);
-			printf("%s< QUINES && MEXIL SHELL >%s\n\n", BCyan, Color_Off);
+			printf("%s< QUINES && MEXIL SHELL >%s\n\n", BCYAN, COLOR_OFF);
 			ft_executor(g_m, g_m->ownenv, &pid);
 			printf("return: %d\n", g_piperet);
-			/*printf("%s< REAL BASH >%s\n\n", BCyan, Color_Off);
-			system(command);*/
 			free_gminishell(g_m);
 		}
 	}

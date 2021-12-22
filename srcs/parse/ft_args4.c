85c7fb6323bc2086d8f50f48ab5eacb696ba6fb4
Mexil77 <mendel_77@hotmail.com>
2021-12-22 13:42:36 +0100

    quitando el print

--- srcs/parse/ft_args4.c
+++ srcs/parse/ft_args4.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 21:01:26 by emgarcia          #+#    #+#             */
-/*   Updated: 2021/12/22 12:52:33 by emgarcia         ###   ########.fr       */
+/*   Updated: 2021/12/22 13:41:56 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,7 +83,6 @@ void	ft_joinenv(char **str, t_general *g)
 			ft_strownjoin(&line, aux);
 			free (aux);
 			last = j;
-			printf("line : %s\n", line);
 			aux = ft_getenv(&str[0][j], &last, &j, g);
 			ft_strownjoin(&line, aux);
 			free (aux);

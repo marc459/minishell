83ec52270e50bd2f3303d3a5ddcd56f1d4920744
Mexil77 <mendel_77@hotmail.com>
2022-01-11 13:51:32 +0100

    Agregar unset de variable sin valor

--- srcs/ft_remenv.c
+++ srcs/ft_remenv.c
@@ -6,7 +6,7 @@
 /*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 06:05:29 by emgarcia          #+#    #+#             */
-/*   Updated: 2022/01/11 13:44:25 by emgarcia         ###   ########.fr       */
+/*   Updated: 2022/01/11 13:49:20 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,27 +31,21 @@ void	ft_remenv(t_general *g, char *remenv)
 	size_t	i;
 	size_t	j;
 
-	if (!ft_checkenvname(remenv))
+	if (!ft_checkenvname(remenv) || !ft_varenvexist(g, remenv))
 	{
-		ft_printf_fd(1, "minishell: syntax error\n");
-		commandoutput(1);
+		printf("unset: `%s': not a valid identifier\n", remenv);
 		return ;
 	}
-	if (!ft_varenvexist(g, remenv))
-	{
-		commandoutput(0);
-		return ;
-	}
-	newenv = calloc(sizeof(char *), ft_splitlen(g->ownenv));
+	newenv = ft_calloc(sizeof(char *), ft_splitlen(g->ownenv));
 	if (!newenv)
 		return ;
 	i = -1;
 	j = 0;
 	while (g->ownenv[++i])
 		if (ft_strncmp(remenv, g->ownenv[i], ft_strlen(remenv))
-			|| g->ownenv[i][ft_strlen(remenv)] != '=')
+			|| (g->ownenv[i][ft_strlen(remenv)] != '='
+			&& g->ownenv[i][ft_strlen(remenv)] != '\0'))
 			newenv[j++] = ft_strdup(g->ownenv[i]);
 	ft_freedouble(g->ownenv);
 	g->ownenv = newenv;
-	commandoutput(0);
 }
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msantos- <msantos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emgarcia <emgarcia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 22:32:27 by marcos            #+#    #+#             */
-/*   Updated: 2022/01/10 19:56:40 by msantos-         ###   ########.fr       */
+/*   Updated: 2022/01/11 13:48:37 by emgarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ void	ft_prompt(t_general *g_m)
 			ft_inigeneral(g_m);
 			ft_parse(g_m, command);
 			//ft_printgeneral(g_m);
-			//printf("%s< QUINES && MEXIL SHELL >%s\n\n", BCYAN, COLOR_OFF);
+			printf("%s< QUINES && MEXIL SHELL >%s\n\n", BCYAN, COLOR_OFF);
 			if (g_m->args)
 				ft_executor(g_m, g_m->ownenv);
 			free_gminishell(g_m);
@@ -70,6 +70,7 @@ int	main(int argc, char **argv)
 	t_general	g_minishell;
 
 	g_piperet = 0;
+	argc = 0;
 	g_minishell.ownenv = ft_ownenv(environ);
 	runcflag(&g_minishell, environ, argv);
 	sig_main();
